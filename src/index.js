f37362226a6f38cddc9acc3893a874a7f6e5900f
stackguardian <mferdous98@aol.com>
2020-09-01 12:29:20 -0400

    deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
+const baseUrl = "http://localhost:3000/dogs"
 
+   const getDogs = () => {
+       fetch(baseUrl)
+       .then(response => response.json())
+       .then(dogs => renderDogs(dogs))
+   }
+
+   const renderDogs = (dogs) => {
+       for(const dog of dogs) {
+           renderDog(dog)
+       }
+   }
+
+   const renderDog = (dog) => {
+       const dogContainer = document.querySelector('#table-body')
+       const dogDiv = document.createElement('div')
+       
+       dogDiv.innerHTML = `
+        <tr><td>${dog.name}</td> <td>*${dog.breed}*</td>
+        <td>*${dog.sex}*</td>
+        <td><button data-id=${dog.id} class="edit-button">Edit</button></td></tr>
+       `
+        dogContainer.append(dogDiv)
+   }
+
+   const clickHandler = () => {
+       document.addEventListener('click', e => {
+        if (e.target.matches('.edit-button'))
+            e.preventDefault()
+            const button = e.target
+            const id = button.dataset.id
+            const dogForm = document.querySelector('#dog-form')
+       })
+   }
+
+clickHandler()
+getDogs()
 })
