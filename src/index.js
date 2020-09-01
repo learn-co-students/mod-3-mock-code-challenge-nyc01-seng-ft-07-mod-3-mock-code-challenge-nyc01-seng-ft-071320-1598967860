c37e98712344167f9c9fec3ec805952542e88236
woink <wardprice@gmail.com>
2020-09-01 10:02:29 -0400

    table is made

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const baseURL = 'http://localhost:3000/dogs/'
 
+  function loadDogs() {
+    fetch(baseURL)
+      .then(resp => resp.json())
+      .then(dogs => dogs.forEach(dog => renderDog(dog)))
+  }
+
+  function renderDog(dog) {
+    const tabelBody = document.querySelector('#table-body')
+    tabelBody.insertAdjacentHTML('afterend', `
+    <tr><td>${dog.name}</td> 
+    <td>${dog.breed}</td> 
+    <td>${dog.sex}</td> 
+    <td><button>Edit</button></td></tr>
+    `)
+  }
+
+  loadDogs()
 })
+
+
+
+
+/*
+
