883f3b35f87b30f7e7cf4569e744e4782977cbfb
Jake Mills <jake.mills.05@gmail.com>
2020-09-01 11:45:28 -0400

    Done. (stress lead to me forgetting to commit more frequently)

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const ce = (tag) => document.createElement(tag)
 
   // GLOBAL VARIABLES
-  const baseUrl = 'http://localhost:3000/dogs'
+  const baseUrl = 'http://localhost:3000/dogs/'
   const tableBody = qs('#table-body')
-
+  const form = qs('form')
+  const name = form.querySelector('input')
+  const breed = form.querySelector('input:nth-child(2)')
+  const sex = form.querySelector('input:nth-child(3)')
 
   // FETCH DOGS
   const getDogs = () => {
@@ -31,12 +34,73 @@ document.addEventListener('DOMContentLoaded', () => {
     <td>${dog.name}</td> 
     <td>${dog.breed}</td> 
     <td>${dog.sex}</td> 
-    <td><button>Edit</button></td>
+    <td><button data-dog-id="${dog.id}">Edit</button></td>
     `
 
     tableBody.append(tr)
   }
 
+  //ADD FUNCTIONALITY TO EDIT BUTTON
+  /* 
+   √ 1. QS THE EDIT BUTTON
+   √ 2. ADD EVENTLISTENER TO EDIT BUTTON
+   √ 3. FIND THE FORM IN REGARDS TO EDIT BUTTON
+   √ 4. POPULATE VALUES OF FORM WITH EDIT BUTTON
+  */
 
+  const clickHandler = () => {
+    tableBody.addEventListener('click', e => {
+      if (e.target.matches('button')) {
+        // const editId = e.target.dataset.dogId
+        const dogTr = e.target.parentElement.parentElement
+
+        form.dataset.dogId = e.target.dataset.dogId
+        name.value = dogTr.querySelector('td').innerText
+        breed.value = dogTr.querySelector('td:nth-child(2)').innerText
+        sex.value = dogTr.querySelector('td:nth-child(3)').innerText
+      }
+    })
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      const dogId = e.target.dataset.dogId
+
+      const options = {
+        method: 'PATCH',
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          "name": `${name.value}`,
+          "breed": `${breed.value}`,
+          "sex": `${sex.value}`
+        })
+      }
+
+      fetch(baseUrl + dogId, options)
+        .then(response => response.json())
+
+      clearForm()
+      tableBody.innerHTML = ""
+
+      getDogs()
+    })
+  }
+
+  const clearForm = () => {
+    name.value = '';
+    breed.value = '';
+    sex.value = '';
+  }
+
+  // PATCH A DOG
+  /*
+   √ 1. ADD EVENT LISTENER TO FORM SUBMIT
+   √ 2. SEND A PATCH REQUEST
+   √ 3. RENDER NEW DOG LIST
+      */
+
+
+  clickHandler()
   getDogs()
 })
