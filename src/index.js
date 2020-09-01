3317e3bded2950b0ceb540cf63198993d8343c57
junko911 <junkotahara911@gmail.com>
2020-09-01 10:07:21 -0400

    Create getDogs function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const DOG_URL = 'http://localhost:3000/dogs/'
+  const ce = (tag) => document.createElement(tag)
+  const qs = (selector) => document.querySelector(selector)
+  const tBody = qs('#table-body')
 
+  //fetch dog data from API
+  const getDogs = () => {
+    fetch(DOG_URL)
+    .then(res => res.json())
+    .then(dogs => dogs.forEach(listDog))
+  }
+
+  //list up the dogs from fetched data
+  const listDog = dog => {
+    const dogTr = ce('tr')
+    tBody.append(dogTr)
+    dogTr.innerHTML = `
+        <td>Dog *Name*</td>
+        <td>*Dog Breed*</td>
+        <td>*Dog Sex*</td>
+        <td><button>Edit</button></td>
+      `
+  }  
+  
+  
+
+
+
+  
+
+  
+  
+  
+  
+  
+  
+  
+  getDogs()
 })
