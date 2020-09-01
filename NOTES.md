0632979d8671f0abcf0fdb18ac160d5a1709da02
amitdesh <amitdesh2014@gmail.com>
2020-09-01 11:20:19 -0400

    need patch updatte and rerender page

---
+++ NOTES.md
@@ -0,0 +1,18 @@
+
+
+1. Render list of dogs on the page
+in mentioned format
+    <!-- 1. GET request
+    http://localhost:3000/dogs
+    2. Render to page in given format
+    `<tr><td>Dog *Name*</td> <td>*Dog Breed*</td> <td>*Dog Sex*</td> <td><button>Edit</button></td></tr>`
+    3. Create Edit Button for each dog -->
+
+2. Edit dog's information using available form
+    <!-- 1. Eventlistener for edit button click
+    2. Load dog info into form -->
+    <!-- 3. Hit submit -->
+    4. send PATCH request to update database
+    5. Re-render the dogs 
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,106 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    fetchDogs()
+    updateDogInfo()
 })
+
+function fetchDogs(){
+    fetch('http://localhost:3000/dogs')
+    .then(resp => resp.json())
+    .then(loopThroughDogs)
+
+}
+
+function loopThroughDogs(dogs){
+    for (dog of dogs){
+        addDogToDOM(dog)
+    }
+}
+
+function addDogToDOM(dog){
+
+    const {name, breed, sex, id} = dog
+
+    let tableBody = document.getElementById('table-body')
+    let tableRow = document.createElement('tr')
+    tableRow.innerHTML = `<tr><td class="name">${name}</td>
+    <td class="breed">${breed}</td>
+    <td class="sex">${sex}</td>
+    <td><button class="edit-button">Edit</button></td></tr>`
+    tableRow.dataset.id = id
+    tableBody.appendChild(tableRow)
+
+}
+
+function updateDogInfo(){
+
+    document.addEventListener('click',function(e){
+
+        if (e.target.matches(".edit-button")){
+            let dogEntry = e.target.parentElement.parentElement
+            console.log(dogEntry)
+            let dogID = dogEntry.dataset.id
+            console.log(dogID)
+            let dogName = dogEntry.querySelector('.name').innerText
+            console.log(dogName)
+            let dogBreed = dogEntry.querySelector('.breed').innerText
+            console.log(dogBreed)
+            let dogSex = dogEntry.querySelector('.sex').innerText
+            console.log(dogSex)
+            editDogData(dogName,dogBreed,dogSex)
+            submitEdittedData(dogID)
+            
+
+        }
+
+    })
+
+}
+
+function editDogData(dogName, dogBreed, dogSex){
+
+    let dogForm = document.querySelector("#dog-form")
+    let dogNameField = dogForm.firstElementChild
+    dogNameField.value = dogName
+    let dogBreedField = dogNameField.nextElementSibling
+    dogBreedField.value = dogBreed
+    let dogSexField = dogBreedField.nextElementSibling
+    dogSexField.value = dogSex
+}
+
+
+function submitEdittedData(dogID){
+    console.log(dogID)
+
+    dogForm = document.querySelector("#dog-form")
+    document.addEventListener('submit',function(e){
+        e.preventDefault()
+        let dogNameField = dogForm.firstElementChild
+        newDogName = dogNameField.value
+        let dogBreedField = dogNameField.nextElementSibling
+        newDogBreed = dogBreedField.value
+        let dogSexField = dogBreedField.nextElementSibling
+        newDogSex = dogSexField.value
+        patchRequest(newDogName,newDogBreed,newDogSex, dogID)
+    })
+
+}
+
+function patchRequest(newDogName,newDogBreed,newDogSex, id){
+    console.log({newDogName,newDogBreed,newDogSex, id})
+    fetch('http://localhost:3000/dogs/'+id,{
+        method: "PATCH",
+        header:{
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: newDogName,
+            breed: newDogBreed,
+            sex: newDogSex
+        })
+    })
+    .then(resp => resp.json())
+    .then(console.log)
+
+}
