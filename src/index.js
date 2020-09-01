b4b7c8d6ca36a2fc973ef17a652f1a01503d2cf4
victorscholz <vgscholz@gmail.com>
2020-09-01 10:25:21 -0400

    creating dog table

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const dogUrl = "http://localhost:3000/dogs"
 
+    const getDogs = () => {
+        fetch(dogUrl)
+        .then(response => response.json())
+        .then(renderDogTable)
+    }
+
+    const dogTable = document.getElementById('#table-body')
+    const renderDogTable = (dogs) => {
+        for(const dog of dogs) {
+            let newDogRow = dogTable.insertRow()
+            let newDogCell = newDogRow.insertCell(0)
+            // name, breed, sex
+            newDogCell.innerText = dog.name
+            newDogCell.innerText = dog.breed
+            newDogCell.innerText = dog.sex
+            newDogCell.dataset.num = dog.id
+            dogTable.append(newDogCell)
+        }
+    }
+    getDogs()
 })
