8bde5b4c26b217deac73403dc6da63a38b745c49
DBenson1017 <devinbenson@gmail.com>
2020-09-01 11:56:22 -0400

    functional uuntil the very end

--- src/index.js
+++ src/index.js
@@ -1,37 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 //DONE render registered dogs in the table 
-//2 edit button shows form with dog info 
+//DONE edit button shows form with dog info 
 //3 submit sends a patch request 
 //4 after submit, new GET request to update the rendered dogs 
 
 let form = document.getElementById('dog-form')
-let fNameP =  form.name.placeholder
-let fBreedP = form.breed.placeholder
-let fSexP = form.sex.placeholder
-
-let fNameV = form.name.value 
-let fBreedV = form.breed.value
-let fSexV = form.sex.value
-
-function updateForm(click){
-    console.log('entered updateForm')
-    let par = click.parentNode
-    let currentSex = par.previousElementSibling
-    let currentBreed = currentSex.previousElementSibling
-    let currentName = currentBreed.previousElementSibling
-
-    console.log(fNameP)
-   
-
-    fNameP = currentName
+let fName = form.name.value
+console.log(fName)
+form.addEventListener('click', function(e){
+    e.preventDefault()
+})
 
 
-    console.log(par.previousElementSibling.innerText)
-    console.log(currentSex.previousElementSibling)
-    console.log(currentBreed.previousElementSibling)
-
- }// end of updateForm
 
 
 
@@ -42,15 +23,77 @@ document.addEventListener('click', function(e){
     if (click.innerText === 'Edit'){
         console.log('edit button clicked')
         updateForm(click)
+
         let par = e.target.parentNode
+        console.log(par)
+        let dId = par.parentNode.id
+        console.log(dId)
+
         let currentSex = par.previousElementSibling
         let currentBreed = currentSex.previousElementSibling
         let currentName = currentBreed.previousElementSibling
+    } else if ( click.value === 'Submit'){
+        console.log('submit click heard')
+        let name = form.name.value
+        let sex = form.sex.value
+        let breed = form.breed.value
+
+
+        console.log(form.name.value)
+        console.log(form.sex.value)
+        console.log(form.breed.value)
+
+        updateDog(breed, name, sex)
+    }
+}) // end of click listener 
+
+function updateDog(breed, name, sex){
+    let dId = 
+
+    let url = 'http://localhost:3000/dogs/:id'
+    
+    let data = {
+        breed: `${form.breed.value}`,
+
+        name: `${form.name.value}`,
+        sex: `${form.sex.value}`
     }
 
+    let configObj = {
+        method: "POST",
+        headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+
+    fetch(`${url}`, configObj)
+    .then(function (response){
+        return response.json
+    })
+    .then (function (newData){
+        console.log(object)
+    })
 
 
-}) // end of click listener 
+    renderDogs()
+}//end of updateDog 
+
+
+
+function updateForm(click){
+    // setting variable from current dog row
+    let par = click.parentNode
+    let currentSex = par.previousElementSibling
+    let currentBreed = currentSex.previousElementSibling
+    let currentName = currentBreed.previousElementSibling
+    //updating form fields from current dog values
+    form.name.value   =  currentName.innerText
+    form.breed.value = currentBreed.innerText
+    form.sex.value = currentSex.innerText
+
+ }// end of updateForm
 
 function renderDogs(id, name, breed, sex){
     let table = document.getElementById('table-body')
@@ -76,19 +119,10 @@ function pullDogs(){
         return response.json()
     })
     .then(function (data){
+        console.log(data)
         iterateDogs(data)
     })
 } // end of pullDogs (GET fetch)
-
-
-
-
-
-
-
-
-
-
 
 
 pullDogs()
