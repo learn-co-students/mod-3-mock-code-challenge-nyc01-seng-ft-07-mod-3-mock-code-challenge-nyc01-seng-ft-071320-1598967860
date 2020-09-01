3f479f5a4c68635f0503b65875165fd2c5cd1ed5
junko911 <junkotahara911@gmail.com>
2020-09-01 10:32:37 -0400

    Set auto fill ion edit form

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const ce = (tag) => document.createElement(tag)
   const qs = (selector) => document.querySelector(selector)
   const tBody = qs('#table-body')
+  const dogForm = qs('#dog-form')
 
   //fetch dog data from API
   const getDogs = () => {
@@ -19,10 +20,40 @@ document.addEventListener('DOMContentLoaded', () => {
         <td>${dog.name}</td>
         <td>${dog.breed}</td>
         <td>${dog.sex}</td>
-        <td><button>Edit Dog</button></td>
+        <td><button class="edit-btn">Edit Dog</button></td>
       `
   }  
   
+  //set autofill in form when edit dog button clicked
+  const clickHandler = () => {
+    document.addEventListener('click', e => {
+      if (e.target.matches('.edit-btn')) {
+        const dogTr = e.target.parentElement.parentElement
+        dogForm.name.value = dogTr.children[0].textContent
+        dogForm.breed.value = dogTr.children[1].textContent
+        dogForm.sex.value = dogTr.children[2].textContent
+      }
+    })
+  }
+
+
+  //set event on submit button
+  const submitDog = () => {
+    qs('#dog-form').addEventListener('submit', e => {
+      e.preventDefault()
+      updateDog(e.target)
+
+
+    })
+  }
+
+  //update dog in API
+  const updateDog = (target) => {
+    console.log(target)
+    // fetch()
+
+
+  }
   
 
 
@@ -37,4 +68,6 @@ document.addEventListener('DOMContentLoaded', () => {
   
   
   getDogs()
+  submitDog()
+  clickHandler()
 })
