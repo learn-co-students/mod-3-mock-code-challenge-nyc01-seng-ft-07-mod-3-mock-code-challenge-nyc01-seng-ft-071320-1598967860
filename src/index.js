da7c536e4a8fdb998409253cab9aef5ea62e310e
woink <wardprice@gmail.com>
2020-09-01 10:17:58 -0400

    rendering edit form correctly

--- src/index.js
+++ src/index.js
@@ -13,10 +13,33 @@ document.addEventListener('DOMContentLoaded', () => {
     <tr><td>${dog.name}</td> 
     <td>${dog.breed}</td> 
     <td>${dog.sex}</td> 
-    <td><button>Edit</button></td></tr>
+    <td><button data-dog-id=${dog.id}>Edit</button></td></tr>
     `)
   }
 
+  function clickHandler() {
+    document.addEventListener('click', e => {
+      const button = e.target
+      if (button.innerText === 'Edit') {
+        const dogId = button.dataset.dogId
+        loadDog(dogId)
+        function loadDog(dogId) {
+          fetch(baseURL + dogId)
+            .then(resp => resp.json())
+            .then(renderEditDogForm)
+        }
+      }
+    })
+  }
+
+  function renderEditDogForm(dogObj) {
+    const dogForm = document.querySelector('#dog-form')
+    dogForm.name.value = dogObj.name
+    dogForm.breed.value = dogObj.breed
+    dogForm.sex.value = dogObj.sex
+  }
+
+  clickHandler()
   loadDogs()
 })
 
@@ -25,3 +48,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
 /*
 
+find button element
+   get values from dog's row 
+  pass to add to value of form 
+
+on submit of form 
+  send patch request to db with new values
+
+form reflects changes added to db
+  make a get request after patch request is sent
+
+  */
